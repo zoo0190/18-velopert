c05ced9c36213d29b71a071a1cd10f07d49d4097
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2021-03-22 23:38:45 +0900

    first commit

---
+++ src/modules/sample.js
@@ -0,0 +1,77 @@
+import { handleActions } from 'redux-actions';
+import createRequestThunk from '../lib/createRequestThunk';
+import * as api from '../lib/api';
+
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+
+// export const getPost = id => async dispatch => {
+//   dispatch({ type: GET_POST });
+//   try {
+//     const response = await api.getPost(id);
+//     dispatch({
+//       type: GET_POST_SUCCESS,
+//       payload: response.data,
+//     });
+//   } catch (e) {
+//     dispatch({
+//       type: GET_POST_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//     throw e;
+//   }
+// };
+
+// export const getUsers = () => async dispatch => {
+//   dispatch({ type: GET_USERS });
+//   try {
+//     const response = await api.getUsers();
+//     dispatch({
+//       type: GET_USERS_SUCCESS,
+//       payload: response.data,
+//     });
+//   } catch (e) {
+//     dispatch({
+//       type: GET_USERS_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//     throw e;
+//   }
+// };
+
+const initialState = {
+  post: null,
+  users: null,
+};
+
+const sample = handleActions(
+  {
+    [GET_POST_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: false,
+      },
+      post: action.payload,
+    }),
+    [GET_USERS_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: false,
+      },
+      users: action.payload,
+    }),
+  },
+  initialState
+);
+
+export default sample;
